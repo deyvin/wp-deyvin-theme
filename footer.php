863c46497e80b2696da5425e791ed38ac8baca61
Deyvid Nascimento <deyvin@gmail.com>
2012-05-05 14:17:19 -0300

    novo footer

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 		<footer>
 			<hr>
-			<i class="icon-bullhorn"></i> Nenhum direito reservado, copie, compartilhe, venda, distribua, hackeie, etc. <?php echo " | "; bloginfo('name'); ?>
+			<i class="icon-bullhorn"></i> Nenhum direito reservado, copie, compartilhe, venda, distribua, hackeie, etc. | <a href="https://github.com/deyvin/wp-deyvin-theme" target="_blank">gostou do tema? Baixe ele aqui!</a>
 		</footer>
 
 	</div>
