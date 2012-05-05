01896c3c3f201ae599a6cf4de095a84d5f02fa95
Deyvid Nascimento <deyvin@gmail.com>
2012-05-05 14:20:58 -0300

    quebrei tema para teste

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 		<div class="row">
 			<div class="span12">
+				<h1>Quebrei meu tema!!!!!!!</h1>
 				<div <?php post_class() ?> id="post-<?php the_ID(); ?>">
 					<h2><a href="<?php the_permalink() ?>"><?php the_title(); ?></a> </h2>
 					<p><?php the_permalink() ?></p>
